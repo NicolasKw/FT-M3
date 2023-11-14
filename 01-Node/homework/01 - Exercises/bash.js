92cc7e4c618a128c6658a9fd204101561db8a067
Nicolas Kwiatkowski <nicolasm.kw@gmail.com>
2023-11-14 13:41:22 -0300

    Correcciones Homework 03 01

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -2,19 +2,19 @@ const process = require('process');
 const { Z_ASCII } = require('zlib');
 const commands = require('./commands/index.js');
 
-function bash() {
-   process.stdout.write('prompt > ');
-   process.stdin.on('data', data => {
-      const args = data.toString().trim();
-      const cmd = args.split(" ")[0];
-      if(commands.hasOwnProperty(cmd)) commands[cmd](print, args);
-      else print(`command not found: ${cmd}`);
-   });
+function print(output) {
+   process.stdout.write(output); //* Imprimo el output
+   process.stdout.write('\nprompt > '); //* Imprimo el salto de línea para que el cliente pueda escribir otro prompt
 }
 
-function print(output) {
-   process.stdout.write(output);
-   process.stdout.write('\nprompt > ');
+function bash() {
+   process.stdout.write('prompt > '); //* Igual que antes pero sin el salto de línea
+   process.stdin.on('data', data => { //* stdin.on es como un AddEventListener para interactuar con la consola
+      let args = data.toString().trim().split(' ');
+      const cmd = args.shift(); //* Quiero que args quede como un array sin el comando
+      if(commands.hasOwnProperty(cmd)) commands[cmd](print, args.join(' '));
+      else print(`command not found: ${cmd}`);
+   });
 }
 
 bash();
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -3,11 +3,11 @@ const utils = require("../utils/request");
 const process = require("process");
 
 function pwd(print) {
-    print(process.cwd());
+    print(process.cwd()); //* cwd = current working directory
 }
 
 function date(print) {
-    print(Date());
+    print(Date()); //* si no le pusiera la D mayúscula estaría haciendo una recursión
 }
 
 function echo(print, args) {
@@ -16,36 +16,36 @@ function echo(print, args) {
 
 function ls(print) {
     fs.readdir('.', (error, files) => {
-        if(error) throw error;
+        if(error) throw new Error(error); //* Se puede poner el new o no, es igual
         print(files.join(' '));
     });
 }
 
 function cat(print, args) {
     fs.readFile(args, 'utf-8', (error, data) => {
-        if(error) throw error;
+        if(error) throw new Error(error);
         print(data);
     })
 }
 
 function head(print, args) {
     fs.readFile(args, 'utf-8', (error, data) => {
-        if(error) throw error;
-        print(data.split('\n')[0]);
+        if(error) throw new Error(error);
+        print(data.split('\n')[0]); //* Si quisiera las primeras 8 líneas, hago split('\n').slice(0, 8) y después un join('\n')
     })
 }
 
 function tail(print, args) {
     fs.readFile(args, 'utf-8', (error, data) => {
-        if(error) throw error;
+        if(error) throw new Error(error);
         const lines = data.split('\n')
-        print(lines[lines.length - 1].trim())
+        print(lines[lines.length - 1].trim()) //* Podría hacer data.split('\n').at(-1) también
     })
 }
 
 function curl(print, args) {
-    utils.request(args, (error, response) => {
-        if(error) throw error;
+    utils.request(`https://${args}`, (error, response) => {
+        if(error) throw new Error(error);
         print(response);
     })
 }
